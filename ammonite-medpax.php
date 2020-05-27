e9104aa34a04c09f44f8c935f40f5a86b3b1eaad
Daniel Ellis <danielshawellis@gmail.com>
2020-05-27 18:49:29 -0500

    Initialize plugin with headers

---
+++ ammonite-medpax.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       Ammonite MedPax
+ * Plugin URI:
+ * Description:
+ * Version:           1.0.0
+ * Requires at least:
+ * Requires PHP:
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ * License:           
+ * License URI:
+ * Text Domain:
+ * Domain Path:
+ */
